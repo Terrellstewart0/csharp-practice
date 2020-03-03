2bb3c3b1dc9808a5061fc91bba1778139a113de0
terrell <terrellstewart0@gmail.com>
2020-03-03 14:39:04 -0600

    Initial check-in of module ThisIsATest

---
+++ ThisIsATest/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ThisIsATest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Bitches!");
+            // This is how you make comments in the terminal
+
+
+
+            //int x = 25;
+            //int y = 75;
+            //Console.WriteLine("x= {0} and y = {1}!", x, y);
+            //Console.Write("both of those combined are ");
+            //Console.WriteLine(x + y);
+            //This is how to do basic maths establish data types
+
+
+            // Console.WriteLine("What do you wanna say?");
+            //String input = Console.ReadLine();
+            // Console.WriteLine("the stupid stuff you said is {0}", input);
+            // Console.WriteLine("Input some extra stuff yo");
+            // String stuff = Console.ReadLine();
+            // Console.WriteLine("The stuff you wrote is {0}", stuff);
+
+            //This is how too take in information and output it to the terminal
+
+            //var num = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("You are {0} year old!", num);
+
+            //This is how to convert datatypes
+
+
+            //var thing = true;
+            //Console.WriteLine(thing);
+            // This is how you use var
+
+            //int x = 5;
+            //int y = 6;
+
+            //if (x >= y)
+            //{
+            //    Console.WriteLine("X is best!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Y is best!");
+            //}
+
+
+            //How to write an if / if-else statement
+
+            //int a = 8;
+            //int b = ++a;
+            //if (a > 5)
+            //    b -= 3;
+            //else
+            //    b = 9;
+            //Console.WriteLine(b);
+            //Example
+
+
+
+        }
+    }
+}
